d68c6fd62eb4a6f7d23bcf477f51a240ff4c8f51
Inunor <antbakatovich@gmail.com>
2021-10-12 06:11:14 +0300

    add body cookie-parser

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(json());
+app.use(cookieParser());
 
 export default app;
