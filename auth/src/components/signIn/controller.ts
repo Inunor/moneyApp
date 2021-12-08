1eb19111ba874c2341198ee0fbeca26f24ab1095
Inunor <antbakatovich@gmail.com>
2021-12-08 06:08:48 +0300

    fix signIn tests

--- auth/src/components/signIn/controller.ts
+++ auth/src/components/signIn/controller.ts
@@ -1,16 +1,24 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { tokensCookieKey } from 'models/token';
 import { UserPayload } from 'models/user';
 
 import { SignInService } from './service';
 
-export const controller = (request: Request, response: Response): void => {
-  const { email, password } = request.body as UserPayload;
+export const controller = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { email, password } = request.body as UserPayload;
 
-  const signInService = new SignInService();
-  const tokens = signInService.signIn({ email, password });
+    const signInService = new SignInService();
+    const tokens = await signInService.signIn({ email, password });
 
-  response.cookie(tokensCookieKey, tokens, { httpOnly: true });
-  response.send({ email, ...tokens });
+    response.cookie(tokensCookieKey, tokens, { httpOnly: true });
+    response.send({ email, ...tokens });
+  } catch (e) {
+    next(e);
+  }
 };
