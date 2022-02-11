63ccdd9d05eaf277d23d2a1b8e8cb9400f04f23f
Inunor <antbakatovich@gmail.com>
2022-02-11 05:22:06 +0300

    add envChecker functionality

--- packages/auth/src/config/index.ts
+++ packages/auth/src/config/index.ts
@@ -1,3 +1,4 @@
 export const ACCESS_TOKEN_LIFE = 120;
 export const REFRESH_TOKEN_SECRET = 'refresh_token_secret';
+export const REFRESH_TOKEN_SECRET_KEY = 'REFRESH_TOKEN_SECRET';
 export const REFRESH_TOKEN_LIFE = 86400;
--- packages/common/src/config/index.ts
+++ packages/common/src/config/index.ts
@@ -1,1 +1,3 @@
 export const ACCESS_TOKEN_SECRET = 'access_token_secret';
+export const ACCESS_TOKEN_SECRET_KEY = 'ACCESS_TOKEN_SECRET';
+export const MONGO_URI_KEY = 'MONGO_URI';
