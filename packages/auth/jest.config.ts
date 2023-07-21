6cf3bb9fd9ac3b438600b71fb9b9fd7c363bd235
Inunor <antbakatovich@gmail.com>
2023-07-21 15:54:17 +0200

    81: common jets config file

--- packages/auth/jest.config.ts
+++ packages/auth/jest.config.ts
@@ -1,36 +1,10 @@
 import type { Config } from '@jest/types';
 
-const coverage: Partial<Config.InitialOptions> = {
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
-  collectCoverageFrom: ['**/*.{js,ts}']
-};
+import { commonConfig } from '../../jest.config';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  verbose: true,
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  },
-  moduleFileExtensions: ['ts', 'js'],
-  modulePaths: ['<rootDir>/src/'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  testMatch: ['**/__test__/**/*.test.(ts|js)'],
-  setupFilesAfterEnv: ['./src/test/setup.ts'],
-  ...coverage
+  ...commonConfig,
+  setupFilesAfterEnv: ['./src/test/setup.ts']
 };
 
 export default config;
--- packages/common/jest.config.ts
+++ packages/common/jest.config.ts
@@ -1,35 +1,9 @@
 import type { Config } from '@jest/types';
 
-const coverage: Partial<Config.InitialOptions> = {
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
-  collectCoverageFrom: ['**/*.{js,ts}']
-};
+import { commonConfig } from '../../jest.config';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  verbose: true,
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  },
-  moduleFileExtensions: ['ts', 'js'],
-  modulePaths: ['<rootDir>/src/'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  testMatch: ['**/__test__/**/*.test.(ts|js)'],
-  ...coverage
+  ...commonConfig
 };
 
 export default config;
